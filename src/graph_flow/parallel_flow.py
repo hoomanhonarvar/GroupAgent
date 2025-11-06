2a1bfeeabf583d21051e6060a57ae079f03d91e6
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2025-11-06 18:44:44 +0330

    creating sample of parallel workflow in autogen

---
+++ src/graph_flow/parallel_flow.py
@@ -0,0 +1,62 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.teams import DiGraphBuilder, GraphFlow
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import os
+from dotenv import load_dotenv
+from autogen_core.models import ModelFamily, ModelInfo
+import asyncio
+from autogen_agentchat.ui import Console
+
+load_dotenv()
+API_KEY=os.getenv("API_KEY")
+BASE_URL=os.getenv("BASE_URL")
+MODEL_NAME=os.getenv("MODEL_NAME")
+
+model_info=ModelInfo(
+    vision=False,
+    function_calling=True,
+    json_output=True,
+    family=ModelFamily.UNKNOWN,
+    structured_output=True,
+    )
+
+model = OpenAIChatCompletionClient(
+        model=MODEL_NAME,
+        api_key=API_KEY,
+        base_url=BASE_URL,
+        model_info=model_info
+    )
+writer=AssistantAgent(
+    "Writer",
+    model_client=model,
+    system_message="""You are a writer. Draft a short paragraph on climate change."""
+)
+editor1 = AssistantAgent("editor1", model_client=model, system_message="Edit the paragraph for grammar.")
+
+editor2 = AssistantAgent("editor2", model_client=model, system_message="Edit the paragraph for style.")
+
+# Create the final reviewer agent
+final_reviewer = AssistantAgent(
+    "final_reviewer",
+    model_client=model,
+    system_message="Consolidate the grammar and style edits into a final version.",
+)
+
+builder=DiGraphBuilder()
+builder.add_node(writer).add_node(editor1).add_node(editor2).add_node(final_reviewer)
+builder.add_edge(writer,editor1)
+builder.add_edge(writer,editor2)
+builder.add_edge(editor1,final_reviewer)
+builder.add_edge(editor2,final_reviewer)
+
+graph=builder.build()
+flow=GraphFlow(
+    participants=builder.get_participants(),
+    graph=graph
+)
+
+
+async def main():
+    await Console(flow.run_stream(task="Write a short paragraph about climate change."))
+
+asyncio.run(main())
