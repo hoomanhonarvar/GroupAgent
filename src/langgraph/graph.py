8263981764dae6e6c7f8bfa5c386f42499d1af4d
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2025-11-23 13:34:30 +0330

    debugging tool calling

--- src/langgraph/graph.py
+++ src/langgraph/graph.py
@@ -4,28 +4,32 @@ from langchain.tools import tool
 from langchain_openai import ChatOpenAI
 from typing_extensions import TypedDict,Literal,Annotated
 import operator
-from langchain.messages import AnyMessage,HumanMessage,SystemMessage
+from langchain.messages import AnyMessage,HumanMessage,SystemMessage,ToolMessage
 from langgraph.graph import START,END,StateGraph
 from langgraph.checkpoint.memory import MemorySaver
 from pydantic import BaseModel,Field
 from tools import *
 from langgraph.types import Command
-from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
-BASEURL=os.getenv("BASE_URL")
-API_KEY=os.getenv("API_KEY")
-MODEL_NAME=os.getenv("MODEL_NAME")
+API_KEY=os.getenv("API_KEY_GPT")
+MODEL_NAME=os.getenv("MODEL_NAME_GPT")
 class user_info(BaseModel):
     username: str = Field(description="The name of the person. if he/she doesn't mention should be null")
     user_intent:Literal["writing","Grammar","vocabulary","None"]|None = Field(description="the Intent of user like writing, Grammar, vocabulary or if he/she doesn't mentioned directly should be None")
     ideal_score: int|None = Field(description="the score target by user if he/she doesn't mention directly 0")
     summary: str|None = Field(description="the summary of user message ")
 
+
+
+
+
 base_model= ChatOpenAI(
     model=MODEL_NAME,
     api_key=API_KEY,
-    base_url=BASEURL,
+    max_tokens=40
+
 )
 class UserIntent(TypedDict):
     user_intent=Literal["writing","Grammar","vocabulary"]
@@ -44,14 +48,13 @@ class tutorials(TypedDict):
 
 
 writing_tools=[]
-Grammar_tools=[correct_grammar,hooman_sentence]
+Grammar_tools=[correct_grammar,print_hello]
 vocabulary_tools=[syn_ant,]
 writing_model=base_model.bind_tools(writing_tools)
-# Grammar_model=base_model.bind_tools(Grammar_tools)
-Grammar_model=create_react_agent(base_model,Grammar_tools)
-# vocabulary_model=base_model.bind_tools(vocabulary_tools)
-vocabulary_model=create_react_agent(base_model,vocabulary_tools)
-
+Grammar_model=base_model.bind_tools(Grammar_tools)
+vocabulary_model=base_model.bind_tools(vocabulary_tools)
+tools_by_name={tool.name:tool for tool in writing_tools+Grammar_tools+vocabulary_tools}
+print(tools_by_name)
 def writing(state:tutorials)->dict:
     if state["stage"]=="writing":
         if "role" in state["messages"][-1] :
@@ -60,23 +63,27 @@ def writing(state:tutorials)->dict:
         structured_output=base_model.with_structured_output(user_info)
 
         search_prompt=f"""
-        In this part user has entered into {state["stage"]} 
-        search if user has changed his/her mind or if he/she wants you to practice
+        In this part you should set your output based on what user exacly says
+        search if user has changed his/her mind
+        the intent of user should be one of writing, Grammar, vocabulary tasks
+
         Human message: {Message_content}
         """
 
         result=structured_output.invoke(search_prompt)
-
+        print(result.user_intent)
         if result.user_intent=="writing"or result.user_intent==None or result.user_intent=="" or result.user_intent=="None":
             prompt=f"""
-                    answer the question of user about {state['messages'][-1]}
+                    you are a helpfull persian assistant for IELTS in  {state["intent"]["user_intent"]} task
+                    answer in farsi the question of user about {state['messages'][-1]}
                     """
             writing_result=base_model.invoke([SystemMessage(content=prompt)])
             state["messages"].append(writing_result.content)
             return state
         elif result.user_intent!="" and result.user_intent!=None and  result.user_intent!="None" :
             prompt=f"""
-              you are a helpfull assistant for IELTS in  {state["intent"]["user_intent"]}
+              you are a helpfull persian assistant for IELTS in  {state["intent"]["user_intent"]}
+              you should talk farsi
               aware user that his/her stage has been changed from {state['stage']} to {result.user_intent}"""
             writing_result=base_model.invoke([SystemMessage(content=prompt)])
             state["messages"].append(writing_result.content)
@@ -93,24 +100,32 @@ def Grammar(state:tutorials)->dict:
         Message_content=state["messages"][-1]
         structured_output=base_model.with_structured_output(user_info)
         search_prompt=f"""
+
         In this part user has entered into {state["stage"]} 
         search if user has changed his/her mind or if he/she wants you to practice
         Human message: {Message_content}
         """
 
-        result=structured_output.invoke(search_prompt)
+        result=structured_output.invoke(search_prompt,{"configurable": {"thread_id": "1"}})
         if result.user_intent=="Grammar" or result.user_intent==None or result.user_intent=="" or result.user_intent=="None":
-            Grammare_prompt=f"""
-                    you are a helpfull assistant for IELTS in  {state['intent']["user_intent"]}
+            Grammar_prompt=f"""
+                    you are a helpful persian assistant for IELTS. in  {state['intent']["user_intent"]}
+                    you should talk in farsi.   
                     answer the question of user: {state['messages'][-1]}
-                    if he/she wants you to correct grammar of sentence you must use correct_grammar tool 
+                    if user wants you to correct grammar of his/her sentence use correct-grammar tool
                     """
-            grammar_model_response=Grammar_model.invoke({"messages": [("system", Grammare_prompt)]})
-            state["messages"].append(grammar_model_response["messages"][-1].content)
+            grammar_model_response=Grammar_model.invoke( Grammar_prompt)
+            state["messages"].append(grammar_model_response.content)
+
+            for toold_call in grammar_model_response.tool_calls:
+                tool=tools_by_name[toold_call["name"]]
+                observation=tool.invoke(toold_call["args"])
+                state["messages"].append(ToolMessage(content=observation, tool_call_id=toold_call["id"]).content)
             return state
         elif result.user_intent!="" and result.user_intent!=None and  result.user_intent!="None" :
             
-            prompt=f""" aware user that his/her stage has been changed from {state['stage']} to {result.user_intent}"""
+            prompt=f""" aware user that his/her stage has been changed from {state['stage']} to {result.user_intent}
+                        you should speak in farsi"""
             grammar_result=base_model.invoke([SystemMessage(content=prompt)])
             state["messages"].append(grammar_result.content)
             state["stage"]=result.user_intent
@@ -131,16 +146,21 @@ def vocabulary(state:tutorials)->dict:
 
         result=structured_output.invoke(search_prompt)
         if result.user_intent=="vocabulary"or result.user_intent==None or result.user_intent=="":
-            voab_prompt=f"""
-                    you are a helpfull assistant for IELTS in  {state["intent"]["user_intent"]}
+            vocab_prompt=f"""
+                    you are a helpfull persian assistant for IELTS in  {state["intent"]["user_intent"]}
                     answer the question of user: {state['messages'][-1]}
                     if he/she wants you to create list of synonyms and antonyms of given word you can use syn-ant tool
                     """
-            vocab_result=vocabulary_model.invoke({"messages": [("system", voab_prompt)]})
-            state["messages"].append(vocab_result["messages"][-1].content)
+            vocab_result=vocabulary_model.invoke([SystemMessage(content=vocab_prompt)])
+            state["messages"].append(vocab_result.content)
+            for toold_call in vocab_result.tool_calls:
+                tool=tools_by_name[toold_call["name"]]
+                observation=tool.invoke(toold_call["args"])
+                state["messages"].append(ToolMessage(content=observation, tool_call_id=toold_call["id"]).content)
             return state
         elif result.user_intent!="" and result.user_intent!=None and  result.user_intent!="None" :
-            prompt=f""" aware user that his/her stage has been changed from {state['stage']} to {result.user_intent}"""
+            prompt=f""" aware user that his/her stage has been changed from {state['stage']} to {result.user_intent}
+                        you should speak in farsi"""
             vocab_result=base_model.invoke([SystemMessage(content=prompt)])
             state["messages"].append(vocab_result.content)
             state["stage"]=result.user_intent
@@ -164,18 +184,23 @@ def greeting(state:tutorials)->dict:
     find this information when directly mentioned in message 
     user_intent should be one of writing, Grammar, vocabulary or None and until user don't mention it, you should not filled it
     ideal score can be analysis from message and should be int you must not use your imagination. when it mentioned by user clearly and some descriptions about it set this score, otherwise it should be 0
+    the highest score is 9 and lowest score is 0
     Human message: {Message_content}
     """
 
     greeting_prompt_intent=f"""
-    you are a helpful assistant for IELTS.
+    
+    you are a helpful persian assistant for IELTS.
+    you should talk in farsi.
     greeting to user and ask for user intent for practice like writing, Grammar, vocabulary or ideal score
     """
 
 
     prompt_score=f"""
-    you are a helpful assistant for IELTS.
+    you are a helpful persian assistant for IELTS.
+    you should talk in farsi.
      ask for user her/his ideal score
+     the highest score is 9 and lowest score is 0
     """
 
 
@@ -189,7 +214,8 @@ def greeting(state:tutorials)->dict:
             state["intent"]["ideal_score"]=llm_answer.ideal_score
             state["intent"]["summary"]=llm_answer.summary
             prompt_recieved_intent=f"""
-            you are a helpful assistant for IELTS.
+            you are a helpful persian assistant for IELTS.
+    you should talk in farsi.
             tanks user for sharing his/her information.
             ask user if he/she wants to pracitce {state["intent"]["user_intent"]} and is her/his ideal score {state["intent"]["ideal_score"]}?
             just to be sure
@@ -205,7 +231,8 @@ def greeting(state:tutorials)->dict:
         state["intent"]["ideal_score"]=llm_answer.ideal_score
         state["intent"]["summary"]=llm_answer.summary
         prompt_recieved_intent=f"""
-        you are a helpful assistant for IELTS.
+        you are a helpful persian assistant for IELTS.
+    you should talk in farsi.
         tanks user for sharing his/her information.
         ask user what does he/she want to pracitce ?
         """
@@ -224,7 +251,6 @@ def greeting(state:tutorials)->dict:
 
 
 def create_workflow():
-
     workflow=StateGraph(tutorials)
     workflow.add_node("greeting",greeting)
     workflow.add_node("writing",writing)
